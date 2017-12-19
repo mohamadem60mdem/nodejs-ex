571da34583a01f089288554a1f8852d85d3e9dfc
mohamadem60mdem <mohamadem60@yahoo.com>
2017-12-18 20:57:06 -0800

    dfs
    fdsfd

--- tests/app_test.js
+++ tests/app_test.js
@@ -30,38 +30,3 @@ describe('Basic routes tests', function() {
     })
 })
 
-
-
-
-const fs = require('fs');
-
-exports.test = "7654765476545461------------";
-
-
- //********************************* this is me
-
-
-// const aaa = require('./mongodb2');
- //console.log(aaa.test);
- 
- 
- 
- 
- 
-  //********************************* this is me
- 
-
-  
-
-
-
-//********************* 
-
-fs.writeFile('./views/index.html', 'Hello Node----------.js', (err) => {
-  if (err) throw err;
-  console.log('The file has been saved!');
-});
-
-
-
-
