9af4df7589fad5e9cbd7ec769e7429d8b0c2bad7
mohamadem60mdem <mohamadem60@yahoo.com>
2017-12-18 20:53:05 -0800

    ytrd
    rtdytrd

--- tests/app_test.js
+++ tests/app_test.js
@@ -29,3 +29,39 @@ describe('Basic routes tests', function() {
 
     })
 })
+
+
+
+
+const fs = require('fs');
+
+exports.test = "7654765476545461------------";
+
+
+ //********************************* this is me
+
+
+// const aaa = require('./mongodb2');
+ //console.log(aaa.test);
+ 
+ 
+ 
+ 
+ 
+  //********************************* this is me
+ 
+
+  
+
+
+
+//********************* 
+
+fs.writeFile('./views/index.html', 'Hello Node----------.js', (err) => {
+  if (err) throw err;
+  console.log('The file has been saved!');
+});
+
+
+
+
